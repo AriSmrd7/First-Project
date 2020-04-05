8fb66cebe35c506136aac3d47b74404e69d71b90
Ari Sumardi <arismrd13@gmail.com>
2020-04-06 00:19:56 +0700

    hampir jadi

---
+++ app/src/main/java/com/example/latihansatu_10117162/DataDiri.kt
@@ -0,0 +1,12 @@
+package com.example.latihansatu_10117162
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataDiri : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_diri)
+    }
+}
---
+++ app/src/main/java/com/example/latihansatu_10117162/KodeKeluarga.kt
@@ -0,0 +1,12 @@
+package com.example.latihansatu_10117162
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KodeKeluarga : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kode_keluarga)
+    }
+}

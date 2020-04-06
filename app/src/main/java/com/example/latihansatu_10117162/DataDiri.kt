967fff4100dade18d5de83f64455b00b2345fc6f
Ari Sumardi <arismrd13@gmail.com>
2020-04-06 11:48:57 +0700

    udah pake validasi

--- app/src/main/java/com/example/latihansatu_10117162/DataDiri.kt
+++ app/src/main/java/com/example/latihansatu_10117162/DataDiri.kt
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class DataDiri : AppCompatActivity() {
 
     private lateinit var button : Button
     private lateinit var nama   : EditText
+    private lateinit var umur   : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_diri)
         button = findViewById(R.id.btnNext)
         nama   = findViewById(R.id.NamaLengkap)
+        umur   = findViewById(R.id.Umur)
 
         button.setOnClickListener{
-            val intent = Intent(this, Selamat::class.java)
-            intent.putExtra("data1", nama.text.toString())
-            startActivity(intent)
+            val warn: String = nama.text.toString()
+            val warn2: String = umur.text.toString()
+
+            if (warn.trim().length>0 && warn2.trim().length>0) {
+                val intent = Intent(this, Selamat::class.java)
+                intent.putExtra("data1", nama.text.toString())
+                startActivity(intent)
+            }else{
+                Toast.makeText(applicationContext,"Tolong isi dulu dengan benar ya! ", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
     }
--- app/src/main/java/com/example/latihansatu_10117162/KodeKeluarga.kt
+++ app/src/main/java/com/example/latihansatu_10117162/KodeKeluarga.kt
@@ -4,17 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class KodeKeluarga : AppCompatActivity() {
 
     private lateinit var button : Button
+    private lateinit var pesan  : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kode_keluarga)
         button = findViewById(R.id.btnMasuk)
+        pesan = findViewById(R.id.Kode)
         button.setOnClickListener{
-            startActivity(Intent(this, DataDiri::class.java))
+            val warn: String = pesan.text.toString()
+            if (warn.trim().length>0) {
+                startActivity(Intent(this, DataDiri::class.java))
+            }else{
+                Toast.makeText(applicationContext,"Tolong masukkin dulu kodenya ya! ",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
